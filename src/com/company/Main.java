a0c3a4237d270cb80434e5d8ac7447de9ab211ae
Michael Donovan <donovanmichaelp@gmail.com>
2022-07-03 17:18:29 -0400

    A few changes following the tutorial. 2022-07-03 5:20P

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -9,5 +9,44 @@ public class Main {
         var now = LocalDateTime.now();
         var monthNumber = now.getMonthValue();
 
+        switch(monthNumber){
+            case 1:
+                System.out.println("january");
+                break;
+            case 2:
+                System.out.println("february");
+                break;
+            case 3:
+                System.out.println("march");
+                break;
+            case 4:
+                System.out.println("april");
+                break;
+            case 5:
+                System.out.println("may");
+                break;
+            case 6:
+                System.out.println("june");
+                break;
+            case 7:
+                System.out.println("july");
+                break;
+            case 8:
+                System.out.println("august");
+                break;
+            case 9:
+                System.out.println("september");
+                break;
+            case 10:
+                System.out.println("octoober");
+                break;
+            case 11:
+                System.out.println("november");
+                break;
+            default:
+                System.out.println("december");
+
+        }
+
     }
 }

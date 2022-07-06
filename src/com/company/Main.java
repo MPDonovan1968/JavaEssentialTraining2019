a4ff21706f3a879b2493cf2c2af3bc5feb217006
Michael Donovan <donovanmichaelp@gmail.com>
2022-07-05 21:38:30 -0400

    A few changes following the tutorial. 2022-07-05 9:38P

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -11,6 +11,16 @@ public class Main {
 
         double result = addValues(i1, i2);
         System.out.println("The sum is " + result);
+
+        double resultOfFour = addValues(i1, i2, i1, i2);
+        System.out.println("The sum of four is " + resultOfFour);
+
+        double resultOfMany = addValues(i1, i2, i1, i2, 22, 34);
+        System.out.println("The sum of Many is " + resultOfMany);
+
+        double resultLast = addValues(55, 12, 8);
+        System.out.println("Last result is: "+ resultLast);
+
     }
 
     private static int getInput(Scanner sc, String prompt) {
@@ -19,7 +29,24 @@ public class Main {
     }
 
     private static double addValues(int i1, int i2) {
+        System.out.println("First Add Values");
         return i1 + i2;
     }
+
+    private static double addValues(int i1, int i2, int i3, int i4) {
+        System.out.println("Values with four");
+        return i1 + i2 + i3 + i4;
+    }
+
+    private static double addValues(int... values) {
+        System.out.println("Indeterminate number of values");
+        int res = 0;
+        for (var value: values
+             ) {
+            res += value;
+        }
+        return res;
+    }
+
 
 }

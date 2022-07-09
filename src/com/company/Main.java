4157c90c103e7254997f7d9e7141279d6d10d330
Michael Donovan <donovanmichaelp@gmail.com>
2022-07-09 19:18:33 -0400

    A few changes following the tutorial. 2022-07-09 7:18P

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,21 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        String s = null;
+
+        try {
+            var sub = s.substring(1);
+        } catch (Exception e) {
+            System.out.println("there was an error");
+            e.printStackTrace();
+//            'Process finished with exit code 0'
+//            Uncomment line below and:
+//            'Process finished with exit code 1'
+            throw new RuntimeException(e);
+
+        }
+        System.out.println("Continuing...");
+
     }
 }

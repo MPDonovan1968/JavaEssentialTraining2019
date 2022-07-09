b5e07f4efc87dd2495c3ead8158f782dd003c8d4
Michael Donovan <donovanmichaelp@gmail.com>
2022-07-09 19:24:49 -0400

    A few changes following the tutorial. 2022-07-09 7:18P

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,18 +4,23 @@ public class Main {
 
     public static void main(String[] args) {
 
-        String s = null;
+//        String s = null;
+        String s = "";
 
         try {
             var sub = s.substring(1);
-        } catch (Exception e) {
+        }catch(NullPointerException e){
+            System.out.println("Null Pointer Exception: " + e.getMessage());
+        }catch(StringIndexOutOfBoundsException e){
+            System.out.println("index out of bounds error " + e.getMessage());
+        }catch (Exception e) {
+            System.out.println("Some other kind of exception");
             System.out.println("there was an error");
             e.printStackTrace();
 //            'Process finished with exit code 0'
 //            Uncomment line below and:
 //            'Process finished with exit code 1'
             throw new RuntimeException(e);
-
         }
         System.out.println("Continuing...");
 

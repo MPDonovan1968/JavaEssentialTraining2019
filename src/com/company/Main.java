e8bae9cf3ef0c53893242e5e712fdb1c07907c20
Michael Donovan <donovanmichaelp@gmail.com>
2022-07-16 16:48:31 -0400

    A few changes following the tutorial. 2022-07-16 4:48p

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,8 +5,25 @@ import java.io.*;
 public class Main {
 
     public static void main(String[] args) {
-        var file = new File("hello.txt");
+        var file = new File("helloH.txt");
         System.out.println("File exists: " + file.exists());
+// try with resources, below. This makes sure that the FileReader and the BufferedReader are closed no matter what happens
+//  with exceptons, etc. FileReader implements the interface Closeable so we are responsible for closing it.
+        try(
+                FileReader reader = new FileReader(file);
+                BufferedReader br = new BufferedReader(reader)
+                ) {
+
+            var text = br.readLine();
+            System.out.println(text);
+        } catch (IOException e) {
+//            throw new RuntimeException(e);
+//            e.printStackTrace();
+            System.out.println("File not found\nCaught error... continuing");
+        }
+        System.out.println("Finishing");
+
+
     }
 
 }

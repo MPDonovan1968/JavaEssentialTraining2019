4b0657ae7e85836d79be6e254b1331117feb2ade
Michael Donovan <donovanmichaelp@gmail.com>
2022-07-26 21:17:34 -0400

    A few changes following the tutorial. 2022-07-26 -  09:17p

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,24 @@
 package com.company;
 
+import com.company.model.ClothingItem;
+
+import java.text.NumberFormat;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        var item = new ClothingItem();
+        item.setType("Shirt");
+        item.setSize("m");
+        item.setPrice(9.79);
+        item.setQuantity(4);
+
+        var totalPrice = item.getPrice() * item.getQuantity();
+        var formatter = NumberFormat.getCurrencyInstance();
+        var output = String.format("Your %s will cost %s",
+                item.getType(),
+                formatter.format(totalPrice));
+        System.out.println(output);
     }
 }

718b5cdda0570b7eb952d83892596ff58c3c120f
Michael Donovan <donovanmichaelp@gmail.com>
2022-08-01 19:38:00 -0400

    A few changes following the tutorial. 2022-08-01 -  07:30p

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,22 +6,39 @@ import com.company.model.Hat;
 import com.company.model.Shirt;
 
 import java.text.NumberFormat;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Main {
 
     public static void main(String[] args) {
 
-        var shirt = new Shirt(
+        Map<String, ClothingItem> items = new HashMap<>();
+
+        items.put("Shirt", new Shirt(
                 ClothingSize.L,
                 19.99,
-                3);
-        displayItemDetails(shirt);
+                3));
 
-        var hat = new Hat(
+        items.put("Hat", new Hat(
                 ClothingSize.M,
                 29.99,
-                1);
-        displayItemDetails(hat);
+                1));
+
+        var anItem = items.get("Hat");
+        displayItemDetails(anItem);
+        anItem =  items.get("Shirt");
+        displayItemDetails(anItem);
+
+        System.out.println("\nor loop:\n");
+
+        var keys = items.keySet();
+        for (String key: keys
+             ) {
+            displayItemDetails(items.get(key));
+        }
+
+
     }
 
     private static void displayItemDetails(ClothingItem item) {

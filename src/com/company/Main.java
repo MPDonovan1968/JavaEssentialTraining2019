457484793b8bdf5b77d5383226637a8991634edf
Michael Donovan <donovanmichaelp@gmail.com>
2022-07-31 20:43:50 -0400

    A few changes following the tutorial. 2022-07-31 -  08:42p

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,6 +1,8 @@
 package com.company;
 
+import com.company.model.ClothingItem;
 import com.company.model.ClothingSize;
+import com.company.model.Hat;
 import com.company.model.Shirt;
 
 import java.text.NumberFormat;
@@ -9,11 +11,22 @@ public class Main {
 
     public static void main(String[] args) {
 
-        var item = new Shirt(
+        var myShirt = new Shirt(
                 ClothingSize.L,
                 19.99,
                 3);
 
+        displayItemDetails(myShirt);
+
+        var myNewHat = new Hat(
+                ClothingSize.XL,
+                12.89,
+                1
+        );
+        displayItemDetails(myNewHat);
+    }
+
+    private static void displayItemDetails(ClothingItem item) {
         var totalPrice = item.getPrice() * item.getQuantity();
         var formatter = NumberFormat.getCurrencyInstance();
         var output = String.format("Your %s %s order will cost %s",
--- src/com/company/model/ClothingSize.java
+++ src/com/company/model/ClothingSize.java
@@ -1,8 +1,8 @@
 package com.company.model;
 
 public enum ClothingSize {
-    S("Small"), M("Medium"), L("Large");
-    private String description;
+    S("Small"), M("Medium"), L("Large"), XL("Extra Large");
+    private final String description;
 
     ClothingSize(String description) {
         this.description = description;
